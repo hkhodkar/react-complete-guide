b26480677f1cc6c35b896b736814c9644f0bbd34
hatef <hatef.khodkar@hotmail.com>
2022-07-21 23:09:28 +0430

    add wrapper component named card to reduce css and repetetive code

--- src/components/ExpensesList.js
+++ src/components/ExpensesList.js
@@ -1,9 +1,10 @@
+import Card from "./card";
 import ExpenseItem from "./ExpenseItem";
 import "./ExpensesList.css";
 
 function ExpensesList(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -24,7 +25,7 @@ function ExpensesList(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
